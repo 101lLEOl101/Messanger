33573eeddb00b5ae5b679aaafed4529f2fd27555
101lLEOl101 <Lenya.kuchuk@gmail.com>
2026-06-13 01:01:59 +0300

    add test coverage

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -3,5 +3,9 @@ module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>'],
   testMatch: ['**/*.test.ts'],
-  moduleFileExtensions: ['ts', 'js', 'json']
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  collectCoverageFrom: ['**/*.ts', '!**/*.test.ts', '!index.ts'],
+  coverageThreshold: {
+    global: {statements: 90, branches: 90, functions: 90, lines: 90},
+  }
 };
